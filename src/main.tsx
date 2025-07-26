e924ec4a77377df482c3800fe43c12d5606276c7
Daliiiiifelah <dalifaleh25@gmail.com>
2025-07-26 08:48:43 +0100

    AFF

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-
+import { ConvexProviderWithAuth, ConvexReactClient } from "convex/react";
+import { auth } from "../convex/auth";
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ConvexProvider client={convex}>
+    <ConvexProviderWithAuth client={convex} auth={auth}>
       <App />
-    </ConvexProvider>
+      </ConvexProviderWithAuth>
   </React.StrictMode>
 );
